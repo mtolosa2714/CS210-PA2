55cbc8453e3fde9450075194006c05fd465e2a50
Mayah Tolosa <mtolosa2714@sdsu.edu>
2026-04-17 02:44:47 -0700

    Commit 1 - Initialize push, pop, top, empty, and size functions

--- ArrayStack.h
+++ ArrayStack.h
@@ -12,25 +12,38 @@ private:
 public:
     void push(const T& value) {
         // TODO
+        //add an element to the stack
+        data.push_back(value);
     }
 
     void pop() {
         // TODO
+        //remove the top element
+        if (empty()) {
+            throw std::out_of_range("Stack is empty");
+        }
+        data.pop_back();
     }
 
     T top() const {
         // TODO
-        return T();
+        //return the top element
+        if (empty()) {
+            throw std::out_of_range("Stack is empty");
+        }
+        return data.back();
     }
 
     bool empty() const {
         // TODO
-        return true;
+        //check if stack is empty
+        return data.empty();
     }
 
     int size() const {
         // TODO
-        return 0;
+        //return number of items
+        return data.size();
     }
 };
 
